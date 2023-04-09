ce0feaa8c381daf1dbed03177e5bd63044038584
AdolfMakarony <adolfmakarony@gmail.com>
2023-04-09 14:07:16 +0300

    Done HW

--- homework/urls.py
+++ homework/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from homework.views import hw_even
+from homework.views import hw_even, hw_sentence_case, hw_lower_case, hw_upper_case, hw_cap_word_case, hw_toggle_case
 
 
 urlpatterns = [
-    path('hw_even_sum', hw_even)
-    # path('even_sum', even)
+    path('hw_even_sum', hw_even),
+    path('change_case/sentence_case', hw_sentence_case),
+    path('change_case/lower_case', hw_lower_case),
+    path('change_case/upper_case', hw_upper_case),
+    path('change_case/capitalize_each_word', hw_cap_word_case),
+    path('change_case/toggle_case', hw_toggle_case),
 ]
--- homework/views.py
+++ homework/views.py
@@ -5,7 +5,7 @@ from homework.my_f import hw_sum_even
 
 
 # Create your views here.
-def hw_even (request: WSGIRequest):
+def hw_even(request: WSGIRequest):
     request_data_dict = dict(request.GET.items())
     a = int(request_data_dict['start'])
     b = int(request_data_dict['end'])
@@ -15,3 +15,74 @@ def hw_even (request: WSGIRequest):
         {'result': hw_sum_even(a, b)}
     )
 
+
+def hw_sentence_case(request: WSGIRequest):
+    if request.method == 'GET':
+        return render(
+            request,
+            'text_change.html',
+            {'text': '-'}
+        )
+    else:
+        request_data = str(dict(request.POST.items())['text']).capitalize()
+        return render(
+            request,
+            'text_change.html',
+            {'text': request_data}
+        )
+def hw_lower_case(request: WSGIRequest):
+    if request.method == 'GET':
+        return render(
+            request,
+            'text_change.html',
+            {'text': '-'}
+        )
+    else:
+        request_data = str(dict(request.POST.items())['text']).lower()
+        return render(
+            request,
+            'text_change.html',
+            {'text': request_data}
+        )
+def hw_upper_case(request: WSGIRequest):
+    if request.method == 'GET':
+        return render(
+            request,
+            'text_change.html',
+            {'text': '-'}
+        )
+    else:
+        request_data = str(dict(request.POST.items())['text']).upper()
+        return render(
+            request,
+            'text_change.html',
+            {'text': request_data}
+        )
+def hw_cap_word_case(request: WSGIRequest):
+    if request.method == 'GET':
+        return render(
+            request,
+            'text_change.html',
+            {'text': '-'}
+        )
+    else:
+        request_data = str(dict(request.POST.items())['text']).title()
+        return render(
+            request,
+            'text_change.html',
+            {'text': request_data}
+        )
+def hw_toggle_case(request: WSGIRequest):
+    if request.method == 'GET':
+        return render(
+            request,
+            'text_change.html',
+            {'text': '-'}
+        )
+    else:
+        request_data = str(dict(request.POST.items())['text']).swapcase()
+        return render(
+            request,
+            'text_change.html',
+            {'text': request_data}
+        )
